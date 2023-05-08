cd7704526c922abbcfe2981bf543e63925d6ab0e
jongsun1993 <jongsun1993@gmail.com>
2023-05-08 17:13:53 +0900

    7week 6 update

--- BOJ/dp/12865_평범한_배낭.cpp
+++ BOJ/dp/12865_평범한_배낭.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 int N, K, dp[100001], temp, W, V;
-set<int> st;
 void fastIO()
 {
     ios_base::sync_with_stdio(false);
@@ -11,26 +10,11 @@ void fastIO()
 int main(){
     fastIO();
     cin >> N >> K;
-    st.insert(0);
-    for (int i = 0; i < N; i++){
+    while(N--){
         cin >> W >> V;
-        vector<pair<int, int>> temp;
-        for (int k : st)
-        {
-            if (k + W <= K){
-                if (dp[k+W] < dp[k] + V){
-                    temp.push_back({k + W, dp[k] + V});
-                }
-            }
-        }
-        for (auto it : temp){
-            dp[it.first] = it.second;
-            st.insert(it.first);
+        for (int i = K; i >= W; i--){
+            dp[i] = max(dp[i], dp[i - W] + V);
         }
     }
-    int ret = 0;
-    for (int v = 1; v <= K; v++){
-        ret = max(dp[v], ret);
-    }
-    cout << ret << '\n';
+    cout << dp[K] << '\n';
 }
